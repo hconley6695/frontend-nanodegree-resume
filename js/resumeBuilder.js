8d4a93c3397e2b97fe3fb3579074158f2bb0e59c
Heather Conley <hconley6695@gmail.com>
2017-01-10 14:02:04 -0500

    using prepend, append, replace to fill in template strings

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,23 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ // $('#main').append('Heather Conley');
+
+ // var awesomeThoughts = "My name is Heather and I am awesome.";
+ // console.log(awesomeThoughts);
+
+
+
+ // var funThoughts = awesomeThoughts.replace('awesome', 'fun');
+
+ // console.log(funThoughts);
+ // $('#main').append(funThoughts);
+var name = "Heather Conley";
+var formattedName = HTMLheaderName.replace("%data%", "Heather Conley");
+
+var role = "Front End Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role;
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
